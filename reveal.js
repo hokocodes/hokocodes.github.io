8ad673dc4799c9f04183a5fceda66ead773fee08
hokole <94093120+hokole@users.noreply.github.com>
2023-05-28 16:25:48 -0500

    Added typewrite animation

--- reveal.js
+++ reveal.js
@@ -1,15 +1,18 @@
 const observer = new IntersectionObserver(entries => {
-    // Loop over the entries
-    entries.forEach(entry => {
-      // If the element is visible
-      if (entry.isIntersecting) {
-        // Add the animation class
-        entry.target.classList.add('image-animation');
-      }
-    });
+  // Loop over the entries
+  entries.forEach(entry => {
+    // If the element is visible
+    if (entry.isIntersecting) {
+      // Add the animation class
+      entry.target.classList.add('image-animation');
+    }
   });
-  
-  const viewbox = document.querySelectorAll('section');
-  viewbox.forEach(image => {
-    observer.observe(image);
-  });
+});
+
+const viewbox = document.querySelectorAll('*');
+viewbox.forEach(image => {
+  observer.observe(image);
+});
+
+
+
